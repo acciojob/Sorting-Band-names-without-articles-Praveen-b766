0c8645322ca5416047ee4fb3c137af4a9ffbd0ce
Praveen-b766 <bandipraveen777@gmail.com>
2023-03-18 12:10:47 +0530

    completed
    completed code

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here
+let bandNames = ['The Beatles', 'Led Zeppelin', 'Pink Floyd', 'The Who', 'Rolling Stones', 'Aerosmith', 'The Doors', 'Nirvana'];
+
+//remove the articles from the band name
+bandNames = bandNames.map(name => name.replace(/^(a|an|the)\s+/i, '').trim());
+
+//sorting the names in lexicographic order
+bandNames.sort();
+
+// select the ul elements
+const ul = document.querySelector('ul');
+
+// Create an array of li elements for the band names and add them to the ul element
+ul.innerHTML = bandNames.map(name => `<li>${name}</li>`).join('');
 
